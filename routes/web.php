55358e8bb3aa35cc7b8d34381cf00f8c19d8e5d1
MartinaGangemi <martinagangemi94@gmail.com>
2022-06-21 15:50:02 +0200

    iniziato a creare lato front

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function(){
     Route::get('/', 'HomeController@index')->name('dashboard');
 });
+
+Route::get("{any?}", function (){
+    return view("guest.home");
+})->where("any",".*");
